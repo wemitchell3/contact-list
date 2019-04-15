3f39a09a279c58987e426df73a1572696f05679a
William Mitchell <wemitchell3@gmail.com>
2019-04-15 15:51:38 -0500

    uploading completed contact list practice

--- src/scripts/eventHandler.js
+++ src/scripts/eventHandler.js
@@ -4,6 +4,11 @@ import contactList from "./contactList"
 const mainContainer = document.querySelector("#display-container")
 
 const eventHandler = {
+    reloadDom() {
+        while (mainContainer.childNodes[1]){
+                mainContainer.removeChild(mainContainer.childNodes[1])
+                contactList.listAllContacts()}
+    },
     handleAddContact() {
         const contactName = document.querySelector("#contact-name-input").value
         const contactPhoneNumber = document.querySelector("#contact-phone-number-input").value
@@ -17,14 +22,11 @@ const eventHandler = {
             "address": contactAddress
         }
 
-        contactCollection.postNewContact(newContact).then(
-            while (mainContainer.childNodes[1])
-                mainContainer.removeChild(mainContainer.childNodes[1])
-            
-        }.then(() => {
-            contactList.listAllContacts()})
-        
+        contactCollection.postNewContact(newContact).then(data => {
+            eventHandler.reloadDom()
+        })
     }
 }
+
 
 export default eventHandler
